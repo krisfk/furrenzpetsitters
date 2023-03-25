66f4da9d46346aa31fea7ee103639e22513af919
kaiyu li <krisfk@gmail.com>
2023-03-25 23:25:17 +0800

    .

--- wp-content/themes/furrenzpetsitters/index.php
+++ wp-content/themes/furrenzpetsitters/index.php
@@ -17,11 +17,11 @@
 get_header(); ?>
 
 <?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-	<header class="page-header alignwide">
-		<h1 class="page-title"><?php single_post_title(); ?></h1>
-	</header><!-- .page-header -->
+<header class="page-header alignwide">
+    <h1 class="page-title"><?php single_post_title(); ?></h1>
+</header><!-- .page-header -->
 <?php endif; ?>
-
+test
 <?php
 if ( have_posts() ) {
 
